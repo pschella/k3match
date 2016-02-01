b2b49f0c1063ca81fabe5f6f24069dfdc5cb6d5d
Pim Schellart <P.Schellart@astro.ru.nl>
2016-02-01 11:43:00 +0100

    Relicense under the Mozilla Public License Version 2.0

--- test/test_k3match.py
+++ test/test_k3match.py
@@ -1,3 +1,10 @@
+# This file is part of K3Match.
+# Copyright (C) 2016 Pim Schellart <P.Schellart@astro.ru.nl>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import numpy as np
 import k3match
 
